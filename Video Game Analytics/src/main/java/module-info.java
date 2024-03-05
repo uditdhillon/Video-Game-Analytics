a1fa12aceb413c5ea1d0f159cb7a1c3f8a3d950a
uditdhillon <148588303+uditdhillon@users.noreply.github.com>
2024-03-05 14:13:07 -0500

    Delete Video Game Analytics directory

--- Video Game Analytics/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.video_game_analytics {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.video_game_analytics to javafx.fxml;
-    exports com.example.video_game_analytics;
-}
